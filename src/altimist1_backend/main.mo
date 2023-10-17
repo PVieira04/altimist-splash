0fd40d72cf9a65f9b62ecbd994386ec659e47c74
Patrick Vieira <patrick@Patricks-MacBook-Air.local>
2023-10-17 12:34:59 +0100

    initial-commit

--- src/altimist1_backend/main.mo
+++ src/altimist1_backend/main.mo
@@ -1,5 +1,90 @@
-actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
-  };
-};
+import Buffer "mo:base/Buffer";
+import Text "mo:base/Text";
+import Debug "mo:base/Debug";
+
+actor email {
+
+	// Create a stable array variable to leverage Orthogonal Persistence.
+	stable var entries : [(Text)] = [];
+
+	// Use a Buffer (mutable datatype) to store email addresses.
+	let emailList = Buffer.fromArray<Text>(entries);
+
+	// Function to add emails to the list.
+	public func addEmail(email : Text) : async Text {
+
+		// Conditional checks if argument 'email' already exists in buffer 'emailList'.
+		if (Buffer.contains<Text>(emailList, email, Text.equal)) {
+			return "This email has already been added to the mailing list."
+		}
+		else {
+			emailList.add(email);
+			return email # " has been added to the email list.";
+		}
+	};
+
+	// Query function to check if email is in list.
+	public query func checkEmail(email : Text) : async Bool {
+		Buffer.contains<Text>(emailList, email, Text.equal)
+	};
+
+	// Query function to display all saved emails in the terminal.
+	public query func showEmails() : async () {
+		if (emailList.size() == 0) {
+			Debug.print("Email list is empty")
+		};
+		Buffer.iterate<Text>(emailList, func (email) {
+  			Debug.print(email);
+		});
+	};
+
+	// Query function to display onr saved email in the terminal. UNDER CONSTRUCTION
+	public query func showEmail() : async Text {
+		if (emailList.size() == 0) {
+			Debug.print("Email list is empty")
+		};
+		return "yes";
+	};
+
+	// Query function to return one email.
+	public query func showOneEmail(email : Text) : async Text {
+		return email
+	};
+
+	// Function to delete emails from mailing list.
+	public func deleteEmail(email : Text) : async Text {
+		if (Buffer.contains<Text>(emailList, email, Text.equal)) {
+			let i = await emailIndex(email : Text);
+			Debug.print(debug_show(i));
+			let x = emailList.remove(i);
+			return x # " has been removed from the mailing list."
+		}
+		else {
+			return email # " does not exist in the mailing list."
+		}
+	};
+
+	// Custom function to generate index from Motoko base library.
+	// Avoids problem with the <?Nat> type.
+	public func emailIndex(email : Text) : async Nat {
+    	let size = emailList.size();
+    	var i : Nat = 0;
+    	while (i < size) {
+    		if (emailList.get(i) == email) {
+    			return i
+    		};
+    	i += 1
+    	};
+		0
+	};
+
+	// Function to run before updates to maintain state.
+	system func preupgrade() {
+		entries := Buffer.toArray<Text>(emailList);
+	};
+
+	// Function to run after updates to maintain state.
+	system func postupgrade() {
+		entries := [];
+	};
+}
--- src/altimist1_frontend/src/index.js
+++ src/altimist1_frontend/src/index.js
@@ -1,19 +1,99 @@
 import { altimist1_backend } from "../../declarations/altimist1_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+document.querySelector("#emailForm").addEventListener("submit", async function(event) {
+	event.preventDefault(); // Stops page refresh upon form submission.
 
-  const name = document.getElementById("name").value.toString();
+	const submitButton = event.target.querySelector("#hidden-submit");
+	submitButton.setAttribute("disabled", true); // Stops user submitting email address more than once.
 
-  button.setAttribute("disabled", true);
+	// Wait for email to be saved before moving on.
+	const email = await document.getElementById("email").value;
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await altimist1_backend.greet(name);
+	// Wait for program to check storage before moving on.
+	const emailCheck = await altimist1_backend.checkEmail(email);
+	let text = "";
+	if (emailCheck) {
+		text = "Your email address was already in the system... Don't worry! You won't get duplicate emails :)";
+	}
+	else {
+		text = "Your email address has now been added to the mailing list.";
+	}
+	
+	document.getElementById("form-status").textContent = "Loading...";
 
-  button.removeAttribute("disabled");
+	await altimist1_backend.addEmail(email);
 
-  document.getElementById("greeting").innerText = greeting;
+	document.getElementById("form-status").textContent = text;
+	document.getElementById("email").value = "";
+	document.getElementById("email").setAttribute("placeholder", "Complete!");
+	document.getElementById("email").setAttribute("disabled", true);
 
-  return false;
+})
+
+// Begin JS logic for disappearing Header.
+
+const hideNavBar = () => {
+	header.classList.remove("nav-show");
+	header.classList.add("nav-hide");
+}
+
+const showNavBar = () => {
+	header.classList.remove("nav-hide");
+    header.classList.add("nav-show");
+}
+
+const timeoutTime = 1000;
+
+const main = document.querySelector("main");
+const header = document.querySelector("header");
+const hoverTriggerArea = document.querySelector(".hover-trigger-area");
+
+let hideNavTimer
+
+let mouseEnterMain = function (event) {
+	main.removeEventListener("mouseenter", mouseEnterMain);
+	header.removeEventListener("mouseleave", mouseLeaveHeader);
+	console.log('cursor enters main area');
+	// Add your code to hide the header after a delay
+	hideNavTimer = setTimeout(function () { hideNavBar() }, timeoutTime);
+};
+
+let mouseLeaveHeader = function (event) {
+	main.removeEventListener("mouseenter", mouseEnterMain);
+	header.removeEventListener("mouseleave", mouseLeaveHeader);
+	console.log('cursor has left the header area');
+	hideNavTimer = setTimeout(function () { hideNavBar() }, timeoutTime);
+}
+
+hoverTriggerArea.addEventListener("mouseenter", function () {
+	console.log('cursor has entered the trigger area');
+    showNavBar();
+
+    // Add the mouseover event listener
+    main.addEventListener("mouseenter", mouseEnterMain);
+	header.addEventListener("mouseleave", mouseLeaveHeader);
 });
+
+header.addEventListener("mouseenter", function() {
+	console.log("user re-entered header. clear the timers")
+	clearTimeout(hideNavTimer);
+	main.addEventListener("mouseenter", mouseEnterMain);
+	header.addEventListener("mouseleave", mouseLeaveHeader);
+})
+
+// End JS logic for disappearing Header.
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Select the hamburger menu and navigation links
+    const hamburgerMenu = document.querySelector(".hamburger-menu");
+    const nav = document.querySelector("nav");
+
+    // Function to toggle the 'active' class on the navigation links
+    const toggleNav = () => {
+        nav.classList.toggle('active');
+    };
+
+    // Add a click event listener to the hamburger menu
+    hamburgerMenu.addEventListener("click", toggleNav);
+});
+
